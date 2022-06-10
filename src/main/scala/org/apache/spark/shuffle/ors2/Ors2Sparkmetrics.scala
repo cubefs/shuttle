50d118d3b83a6e3377533bcab47da24961894b09
bigbigxu <xuen.oppo.com>
2022-06-10 17:15:59 +0800

    shuttle: Fix the bug of shuffle read metric writing

--- src/main/scala/org/apache/spark/shuffle/ors2/Ors2Sparkmetrics.scala
+++ src/main/scala/org/apache/spark/shuffle/ors2/Ors2Sparkmetrics.scala
@@ -4,25 +4,16 @@ import org.apache.spark.executor.{ShuffleReadMetrics, ShuffleWriteMetrics}
 import org.apache.spark.shuffle.{ShuffleReadMetricsReporter, ShuffleWriteMetricsReporter}
 
 case class Ors2ShuffleReadMetrics(reporter: ShuffleReadMetricsReporter) extends ShuffleReadMetrics {
-
-  override private[spark] def incRemoteBlocksFetched(v: Long): Unit = {
-    reporter.incRemoteBlocksFetched(v)
-  }
-
-  override private[spark] def incLocalBlocksFetched(v: Long): Unit = {
-    reporter.incLocalBlocksFetched(v)
-  }
-
   override private[spark] def incRemoteBytesRead(v: Long): Unit = {
     reporter.incRemoteBytesRead(v)
   }
 
-  override private[spark] def incRemoteBytesReadToDisk(v: Long): Unit = {
-    reporter.incRemoteBytesReadToDisk(v)
+  override def incFetchWaitTime(v: Long): Unit = {
+    reporter.incFetchWaitTime(v)
   }
 
-  override private[spark] def incLocalBytesRead(v: Long): Unit = {
-    reporter.incLocalBytesRead(v)
+  override private[spark] def incRecordsRead(v: Long):Unit = {
+    reporter.incRecordsRead(v)
   }
 }
 
@@ -39,12 +30,5 @@ case class Ors2ShuffleWriteMetrics(reporter: ShuffleWriteMetricsReporter) extend
     reporter.incWriteTime(v)
   }
 
-  override private[spark] def decBytesWritten(v: Long): Unit = {
-    reporter.decBytesWritten(v)
-  }
-
-  override private[spark] def decRecordsWritten(v: Long): Unit = {
-    reporter.decRecordsWritten(v)
-  }
 }
 
